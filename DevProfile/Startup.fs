b46eb798b4206c4fda5506997237321447fc253a
abnerfs <abnerfs@gmail.com>
2021-02-18 21:56:40 -0300

    enable developer page

--- DevProfile/Startup.fs
+++ DevProfile/Startup.fs
@@ -44,8 +44,8 @@ type Startup(configuration: IConfiguration) =
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member _.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
-        if (env.IsDevelopment()) then
-            app.UseDeveloperExceptionPage() |> ignore
+        //if (env.IsDevelopment()) then
+        app.UseDeveloperExceptionPage() |> ignore
 
         app.UseRouting()
            .UseAuthorization()
